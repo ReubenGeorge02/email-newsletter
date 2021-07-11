f6d78e9edd2da67dd0edc3cfede36045e99d77d4
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-07-11 11:17:51 +0530

    attachments

--- newsletter.py
+++ newsletter.py
@@ -2,6 +2,10 @@ import smtplib
 import ssl
 from string import Template
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
@@ -32,11 +36,27 @@ message_template = get_template("template.txt")
 s = smtplib.SMTP_SSL(host="smtp.gmail.com", port=465)
 s.login(user=my_address, password=password)
 
+p = MIMEBase('application', 'octet-stream')
+
+if ((input("Attach files? (y/n): ")).lower() == "y"):
+    attachment = open(
+        input("Enter the path of the attachment file: "), mode='rb')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition',
+                 "attachment; filename= %s" % attachment)
+else:
+    attachment = False
+
+
 for i in range(len(names)):
     message_body = message_template.substitute(PERSON_NAME=names[i].title())
-    msg = MIMEText(message_body, "html")
+    msg = MIMEMultipart()
+    msg.attach(MIMEText(message_body, 'html'))
     msg["Subject"] = "this is a test email"
     msg["From"] = my_address
     msg["To"] = emails[i]
+    if(attachment):
+        msg.attach(p)
     s.sendmail(my_address, emails[i], msg.as_string())
 s.quit()
